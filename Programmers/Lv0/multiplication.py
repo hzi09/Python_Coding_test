fe3942df48c496dbeba780a672d93363a775a4ba
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-11-06 23:56:30 +0900

    Update multiplication.py

--- Programmers/Lv0/multiplication.py
+++ Programmers/Lv0/multiplication.py
@@ -2,5 +2,5 @@
 # 정수 num1, num2가 매개변수로 주어질 때, num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
 
 def solution(num1, num2) :
-    answer = num1 * num2
+    answer = int(num1 * num2)
     return answer
